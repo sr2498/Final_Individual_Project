b7973d2d2df3cd2fc35b0902a47935af2aae971c
Sandhya Rani <sr2498@njit.edu>
2023-08-09 23:02:50 -0400

    Added Workflow

--- .next/static/webpack/webpack.7b1ddcf701a7d8ef.hot-update.js
+++ .next/static/webpack/webpack.7b1ddcf701a7d8ef.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c1d597fda4ba3630"; }
+/******/ 	__webpack_require__.h = function() { return "b7a64ab695250555"; }
 /******/ }();
 /******/ 
 /******/ }
