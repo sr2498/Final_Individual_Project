64c2ab3c16b2f937bdef813a21392f3c28824cdd
Sandhya <134464080+sr2498@users.noreply.github.com>
2023-08-10 20:43:52 -0400

    Update footer.js
    Updated linkedin and Github URLs

--- components/footer.js
+++ components/footer.js
@@ -8,16 +8,16 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.socialLinks}>
         {/* Replace 'your-linkedin-url', 'your-facebook-url', and 'your-github-url' with your actual profile URLs */}
-        <a href='your-linkedin-url' target='_blank' rel='noopener noreferrer'>
+        <a href='https://www.linkedin.com/in/sandhya-r-928097226/' target='_blank' rel='noopener noreferrer'>
           <img src='images/Linkdin logo.png' alt='LinkedIn' />
         </a>
-        <a href='your-facebook-url' target='_blank' rel='noopener noreferrer'>
+        <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'>
           <img src='images/facebook.png' alt='Facebook' />
         </a>
-        <a href='your-github-url' target='_blank' rel='noopener noreferrer'>
+        <a href='https://github.com/sr2498' target='_blank' rel='noopener noreferrer'>
           <img src='images/Github Logo.png' alt='GitHub' />
         </a>
-        <a href='your-twitter-url' target='_blank' rel='noopener noreferrer'>
+        <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
           <img src='images/twitter.png' alt='Twitter' />
         </a>
       </div>
